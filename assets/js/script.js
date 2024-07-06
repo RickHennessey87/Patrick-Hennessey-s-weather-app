ee61d0d4d2ec3d9698aa7b90544b8e8026803341
Patrick Hennessey <pjhennessey89@gmail.com>
2024-07-06 09:01:22 -0500

    Worked on function to get coordinates

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,20 +6,39 @@ $(document).ready(() => {
     const apiKey = '2bb98df7c6bd4e1372efffedf391c44c';
     const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-    renderHistory();
-});
+    // renderHistory();
 
-form.on('submit', (event) => {
-    event.preventDefault();
+    form.on('submit', (event) => {
+        event.preventDefault();
+        const city = cityInput.val().trim();
 
-    const city = cityInput.val().trim();
+        if (city) {
+            retrieveCoordinates(city);
 
-    if (city) {
-        retrieveCoordinates(city);
+            cityInput.val('');
+            }
+    });
 
-        cityInput.val('');
+    const retrieveCoordinates = (city) => {
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Can't find city Coordinates.")
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.length === 0) {
+                    throw new Error('City not found');
+                }
+                const { lat, lon } = data[0];
+                
+                // getWeather(lat, lon, city);
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
-
 });
-
-retrieveCoordinates (city)
