c023298f54bc8fea93001751b191a42d299b56d9
Patrick Hennessey <pjhennessey89@gmail.com>
2024-07-06 08:11:19 -0500

    Added prelimnary Javascript like variables, page load and form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(() => {
-    
-})
+    const form = $('.form');
+    const cityInput = $('#city-input');
+    const history = $('#search-history');
+    const forecast = $('#forecast-content');
+    const apiKey = '2bb98df7c6bd4e1372efffedf391c44c';
+    const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+
+    renderHistory();
+});
+
+form.on('submit', (event) => {
+    event.preventDefault();
+
+    const city = cityInput.val().trim();
+
+    if (city) {
+        retrieveCoordinates(city);
+
+        cityInput.val('');
+    }
+
+});
+
+retrieveCoordinates (city)
